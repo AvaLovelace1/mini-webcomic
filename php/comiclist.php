b563b9bef66fa6e9c1d301aa5b09d7a86ba9f61d
AvaLovelace1 <flatthefish@gmail.com>
2021-06-11 21:31:21 -0400

    Add support for dates

--- php/comiclist.php
+++ php/comiclist.php
@@ -4,11 +4,12 @@
   
     // Class for a single page of a comic.
     class Comic {
-        var $title, $alt, $hover, $url, $page, $totalPages;
-        function __construct($title, $hover, $url, $page, $totalPages) {		
+        var $title, $alt, $hover, $date, $url, $page, $totalPages;
+        function __construct($title, $hover, $date, $url, $page, $totalPages) {
             $this->title = $title;
             $this->alt = "Comic " . title . " page " . $page; // Sets alt text to "Comic <comic title> page <page number>"
             $this->hover = $hover; // Sets title-text
+            $this->date = $date; // Date published
             $this->url = IMAGE_DIRECTORY . $url;
             $this->page = $page; // Number of this comic page
             $this->totalPages = $totalPages; // Total number of pages in this comic's storyline
@@ -28,9 +29,11 @@
     foreach ($comics as $story) {
         $data = array_map("trim", explode("::", $story));
         $hovers = array_map("trim", explode(",,", $data[1]));
-        $url = explode(".", $data[2]);
-        for ($i = 0; $i < $data[3]; $i++) {
-            $comicList[] = new Comic($data[0], $hovers[$i], $url[0] . "_" . str_pad(($i + 1), 3, '0', STR_PAD_LEFT) . "." . $url[1], $i + 1, $data[3]);
+        $dates = array_map("trim", explode(",,", $data[2]));
+        $url = explode(".", $data[3]);
+        for ($i = 0; $i < $data[4]; $i++) {
+            if ($dates[$i] > date("Y-m-d")) break;
+            $comicList[] = new Comic($data[0], $hovers[$i], $dates[$i], $url[0] . "_" . str_pad(($i + 1), 3, '0', STR_PAD_LEFT) . "." . $url[1], $i + 1, $data[4]);
         }
     }
     
