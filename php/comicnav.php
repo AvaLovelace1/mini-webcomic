a2f262297097a6c096a23f5f5e2f891ee7e039d3
AvaLovelace1 <flatthefish@gmail.com>
2021-10-09 14:08:57 -0400

    Adjust nav spacing

--- php/comicnav.php
+++ php/comicnav.php
@@ -9,20 +9,22 @@ include_once("./php/database.php");
  */
 ?>
 
-<div class="row text-center">
-    <div class="col">
-        <a href="index.php?comic=<?= $first_page ?>" class="btn btn-light" role="button">First</a>
-    </div>
-    <div class="col">
-        <a href="index.php?comic=<?= $prev_page ?>" class="btn btn-light" role="button">Prev</a>
-    </div>
-    <div class="col">
-        <a href="index.php?comic=<?= $random_page ?>" class="btn btn-light" role="button">Random</a>
-    </div>
-    <div class="col">
-        <a href="index.php?comic=<?= $next_page ?>" class="btn btn-light" role="button">Next</a>
-    </div>
-    <div class="col">
-        <a href="index.php?comic=<?= $last_page ?>" class="btn btn-light" role="button">Last</a>
+<div class="container p-0">
+    <div class="row text-center g-0">
+        <div class="col">
+            <a href="index.php?comic=<?= $first_page ?>" class="btn btn-light" role="button">First</a>
+        </div>
+        <div class="col">
+            <a href="index.php?comic=<?= $prev_page ?>" class="btn btn-light" role="button">Prev</a>
+        </div>
+        <div class="col">
+            <a href="index.php?comic=<?= $random_page ?>" class="btn btn-light" role="button">Random</a>
+        </div>
+        <div class="col">
+            <a href="index.php?comic=<?= $next_page ?>" class="btn btn-light" role="button">Next</a>
+        </div>
+        <div class="col">
+            <a href="index.php?comic=<?= $last_page ?>" class="btn btn-light" role="button">Last</a>
+        </div>
     </div>
 </div>
