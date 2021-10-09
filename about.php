6165d489670aef4296a514e62537aa43735a7df4
AvaLovelace1 <flatthefish@gmail.com>
2021-10-09 14:47:17 -0400

    Add about page

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Mini Webcomic - About</title>
+    <?php include_once("php/head.php") ?>
+</head>
+
+<body>
+
+<?php include_once("php/header.php") ?>
+
+<section id="comic" class="my-3 container">
+    <div class="row justify-content-center">
+        <div class="col m-3">
+            <h1>About</h1>
+            <p>Hi! I’m me, and this is my awesome webcomic!</p>
+        </div>
+    </div>
+</section>
+
+<?php include_once("php/footer.php") ?>
+<?php include_once("php/tail.php") ?>
+
+</body>
+</html>
--- php/header.php
+++ php/header.php
@@ -12,10 +12,10 @@
                     <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Archive</a>
+                    <a class="nav-link" href="about.php">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="#">Archive</a>
                 </li>
             </ul>
         </div>
